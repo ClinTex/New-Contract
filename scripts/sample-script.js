f0e8da97f7a5b9c3a4d50a00d43dd2585979cdae
IntegralTeam <sfxdx@sfxdx.com>
2021-10-11 21:42:16 +0300

    Initial commit

---
+++ scripts/sample-script.js
@@ -0,0 +1,16 @@
+
+const {ethers} = require("hardhat");
+
+async function main() {
+
+  const ClinTex = await ethers.getContractFactory("ClinTex");
+  const clintex = await ClinTex.deploy();
+  console.log("ClinTex deployed to address:", clintex.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
